3ecad5393e63db5973b8bb1e6e1169d1cc12f81d
Francisco Bravo <francisco.bravoleiva@externos-cl.cencosud.com>
2024-05-14 23:30:31 -0400

    se quito React.StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,13 +7,11 @@ import { SnackbarProvider } from 'notistack';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <HelmetProvider>
       <SnackbarProvider>
         <App />
       </SnackbarProvider>
     </HelmetProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
